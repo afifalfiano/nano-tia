1bd85f86bc787a306be8cbbdeb52c3476231bbdc
Afif Alfiano <m.afif@cakap.id>
2024-06-01 19:02:31 +0700

    feat(lib): add lib bootstrap, react-helmet, react-router-dom and redux-toolkit

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
+          <button class="btn btn-primary">Test</button>
         </a>
       </header>
     </div>
